1e850baecf77f8139aa8feeddf168f34b38d7053
avrahamGol <avragol@gmail.com>
2023-04-17 14:59:07 +0300

    carousel

---
+++ src/components/CarouselComp.jsx
@@ -0,0 +1,107 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import axios from 'axios';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const CarouselComp = () => {
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [cardsState, setCardsState] = React.useState(null)
+    const [maxSteps, setMaxSteps] = React.useState(null)
+    React.useEffect(() => {
+        (async () => {
+            const { data } = await axios.get("/cards/cards")
+            setCardsState(data);
+            setMaxSteps(data.length)
+        })()
+    }, [])
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    };
+
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
+
+    const handleStepChange = (step) => {
+        setActiveStep(step);
+    };
+
+    if (!cardsState) {
+        return ""
+    }
+
+    return (
+        <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+            <Paper
+                square
+                elevation={0}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    height: 50,
+                    pl: 2,
+                    bgcolor: 'background.default',
+                }}
+            >
+                <Typography>{cardsState[activeStep].label}</Typography>
+            </Paper>
+            <AutoPlaySwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index={activeStep}
+                onChangeIndex={handleStepChange}
+                enableMouseEvents
+            >
+                {cardsState.map((step, index) => (
+                    <div key={step.label}>
+                        {Math.abs(activeStep - index) <= 2 ? (
+                            <CarouselComp card={step} />
+                        ) : null}
+                    </div>
+                ))}
+            </AutoPlaySwipeableViews>
+            <MobileStepper
+                steps={maxSteps}
+                position="static"
+                activeStep={activeStep}
+                nextButton={
+                    <Button
+                        size="small"
+                        onClick={handleNext}
+                        disabled={activeStep === maxSteps - 1}
+                    >
+                        Next
+                        {theme.direction === 'rtl' ? (
+                            <KeyboardArrowLeft />
+                        ) : (
+                            <KeyboardArrowRight />
+                        )}
+                    </Button>
+                }
+                backButton={
+                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+                        {theme.direction === 'rtl' ? (
+                            <KeyboardArrowRight />
+                        ) : (
+                            <KeyboardArrowLeft />
+                        )}
+                        Back
+                    </Button>
+                }
+            />
+        </Box>
+    );
+}
+
+export default CarouselComp;
